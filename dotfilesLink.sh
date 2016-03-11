1aa49ab68a13ed2abc8b04ea962033762f25f882
kanbekuroda <88towtnb+git@gmail.com>
2016-03-11 23:50:49 +0900

    Add .slate

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.Rprofile ~/.Rprofile
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.slate ~/.slate
