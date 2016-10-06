ea5a3476b7776cbaa158c594606cd63f31587b26
kanbekuroda <88towtnb+git@gmail.com>
2016-10-06 19:34:47 +0900

    Add .Renviron

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.Rprofile ~/.Rprofile
+ln -sf ~/dotfiles/.Renviron ~/.Renviron
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
